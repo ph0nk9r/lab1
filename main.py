40b8a62e9445b5187f9ff3a18fb2b0448bc2d156
Tomen Y <yura.2002.tomen@gmail.com>
2024-11-23 23:58:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class movie:
+        def __init__(self, name: str = "", Leading_actors: list = [], Duration: int = 0, Studio: str = "", Year_of_release: int = 0, MPAA_rating: float = 0, 
+                     Genre: list = [], Country_of_production: str = ""):
+                self.__name = name
+                self.__Leading_actors = Leading_actors
+                self.__Duration = Duration
+                self.__Studio = Studio
+                self.__Year_of_release = Year_of_release
+                self.__MPAA_rating = MPAA_rating
+                self.__Genre = Genre
+                self.__Country_of_production = Country_of_production
+
+                
